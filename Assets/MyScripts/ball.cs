9f802f0ef364d1f65ad2e83ff852596cbb490d28
erikm0111 <erikmatosevic@gmail.com>
2017-06-05 20:11:39 +0200

    first

---
+++ Assets/MyScripts/ball.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
+
+public class ball : NetworkBehaviour {
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "Goal")
+        {
+            Destroy(gameObject, .2f);
+            Debug.Log("You win!");
+            //SceneManager.LoadScene("level2");
+            //NetworkManager.singleton.ServerChangeScene("level2");
+            NetworkManager.Shutdown();
+            NetworkManager.singleton.ServerChangeScene("mainmenu");
+        }
+    }
+
+}
---
+++ Assets/MyScripts/tilt.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+public class tilt : NetworkBehaviour {
+
+    public Vector3 currentRot;
+    public GameObject spherePrefab;
+    public Transform sphereSpawn;
+
+	// Use this for initialization
+	void Start () {
+        GameObject sphere = (GameObject)Instantiate(spherePrefab, sphereSpawn.position, sphereSpawn.rotation);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+        currentRot = GetComponent<Transform>().eulerAngles;
+
+        if ((Input.GetAxis("Horizontal") > .2) && (currentRot.z <= 10 || currentRot.z >= 348))
+        {
+            transform.Rotate(0, 0, 1);
+        }
+        if ((Input.GetAxis("Horizontal") < -.2) && (currentRot.z >= 349 || currentRot.z <= 11))
+        {
+            transform.Rotate(0, 0, -1);
+        }
+        if ((Input.GetAxis("Vertical") > .2) && (currentRot.x <= 10 || currentRot.x >= 348))
+        {
+            transform.Rotate(1, 0, 0);
+        }
+        if ((Input.GetAxis("Vertical") < -.2) && (currentRot.x >= 349 || currentRot.x <= 11))
+        {
+            transform.Rotate(-1, 0, 0);
+        }
+    }
+
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.blue;
+    }
+}
