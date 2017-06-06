c668c1e14751c0a566ea11b640a296fb87d4a5ec
erikm0111 <erikmatosevic@gmail.com>
2017-06-06 10:25:43 +0200

    added menus

---
+++ Assets/MyScripts/btnstartgame.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class btnstartgame : MonoBehaviour
+{
+
+    public Button btnPlay;
+
+    // Use this for initialization
+    void Start()
+    {
+        Button btn = btnPlay.GetComponent<Button>();
+        btn.onClick.AddListener(LoadMenu);
+    }
+
+    void LoadMenu()
+    {
+        SceneManager.LoadScene("realDeal");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
